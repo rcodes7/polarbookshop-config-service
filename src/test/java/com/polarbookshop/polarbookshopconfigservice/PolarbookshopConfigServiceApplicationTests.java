c7760d9cb66b41a0d1074ec646b4c8212c64e542
raul <123352622+rcodes7@users.noreply.github.com>
2023-02-20 16:52:39 -0600

    initial project

---
+++ src/test/java/com/polarbookshop/polarbookshopconfigservice/PolarbookshopConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.polarbookshopconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarbookshopConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
