8e913ae8f1d579ff04f202d57c8aaf63ac0260df
raul <rrdev.9@gmail.com>
2023-02-20 16:53:07 -0600

    enable config server

--- src/main/java/com/polarbookshop/polarbookshopconfigservice/PolarbookshopConfigServiceApplication.java
+++ src/main/java/com/polarbookshop/polarbookshopconfigservice/PolarbookshopConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.polarbookshop.polarbookshopconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PolarbookshopConfigServiceApplication {
 
 	public static void main(String[] args) {
