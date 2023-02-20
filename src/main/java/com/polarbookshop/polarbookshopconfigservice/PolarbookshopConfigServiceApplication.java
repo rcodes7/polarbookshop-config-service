c2ee139f30aed9fc9ffe6498c4a014e3b1627b5e
raul <rrdev.9@gmail.com>
2023-02-20 16:52:39 -0600

    initial project

---
+++ src/main/java/com/polarbookshop/polarbookshopconfigservice/PolarbookshopConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.polarbookshopconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarbookshopConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarbookshopConfigServiceApplication.class, args);
+	}
+
+}
